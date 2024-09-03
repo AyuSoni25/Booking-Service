300581a10ef282196e33c8076774bdb936f95030
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-09-03 17:22:22 +0530

    Feat: Added booking logic with transactions

---
+++ src/routes/v1/booking.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { BookingController } = require('../../controllers');
+
+const bookingRouter = express.Router();
+
+bookingRouter.post(
+    '/',
+    BookingController.createBooking
+)
+
+
+module.exports = bookingRouter;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { InfoController } = require('../../controllers');
+const bookingRouter = require('./booking.routes');
 
 const v1Router = express.Router();
 
 v1Router.get('/info', InfoController.info);
+v1Router.get('/bookings', bookingRouter)
+
 
 module.exports = v1Router;
